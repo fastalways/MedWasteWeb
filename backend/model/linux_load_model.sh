e2a1ee5768ba79943cccd151281b2f279fae4334
fastalways <pongsagorn.ch@gmail.com>
2022-06-24 16:16:48 +0700

    modified dl script

--- backend/model/linux_load_model.sh
+++ backend/model/linux_load_model.sh
@@ -1,8 +1,21 @@
-$ echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
-$ wget https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip
-$ echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
-$ wget https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip
-$ echo 'Model Unzipping..................'
-$ tar -xvf 4G-EfficientNetB5-epoch2000.pb.zip
-$ tar -xvf 41CLASSES-EfficientNetB5-epoch2000.pb.zip
-$ echo '++++++++++ Completed ++++++++++'
+#!/bin/bash
+
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
+wget https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
+wget https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
+wget https://assets.gezdev.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip'
+wget https://assets.gezdev.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip'
+wget https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip
+echo 'Model Unzipping..................'
+tar -xzvf '4G-EfficientNetB5-epoch2000.pb.zip'
+tar -xzvf '41CLASSES-EfficientNetB5-epoch2000.pb.zip'
+tar -xzvf '10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
+tar -xzvf 'yolo_v4_41CLASSES.zip'
+ls -a
+Model_MedWaste_Path = $(pwd)
+echo 'Model Saved at ...' + $Model_MedWaste_Path
+echo '++++++++++ [Load/Extract Model] Completed ++++++++++'
--- backend/model/windows_load_model.ps1
+++ backend/model/windows_load_model.ps1
@@ -1,11 +1,20 @@
-echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
 $client = new-object System.Net.WebClient
 $CUR_DIR = Get-Location
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
 $client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip",($CUR_DIR.Path+"\4G-EfficientNetB5-epoch2000.pb.zip"))
 echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
 $client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip",($CUR_DIR.Path+"\41CLASSES-EfficientNetB5-epoch2000.pb.zip"))
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
+$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip",($CUR_DIR.Path+"\10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip"))
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip'
+$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip",($CUR_DIR.Path+"\yolo_v4_41CLASSES.zip"))
+echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip'
+$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip",($CUR_DIR.Path+"\yolo_v5_41CLASSES.zip"))
 echo 'Model Unzipping..................'
 Expand-Archive '4G-EfficientNetB5-epoch2000.pb.zip'
 Expand-Archive '41CLASSES-EfficientNetB5-epoch2000.pb.zip'
+Expand-Archive '10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
+Expand-Archive 'yolo_v4_41CLASSES.zip'
+ls
 echo ('Model Saved at ...' + $CUR_DIR.Path)
 echo '++++++++++ [Load/Extract Model] Completed ++++++++++'
