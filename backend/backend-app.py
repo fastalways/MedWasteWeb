0d2e3d78747aca076030ebb5aaaf4a5d4be94541
GHOME-PC <pongsagorn.ch@gmail.com>
2022-07-01 05:46:02 +0700

    fixed cross-origin again again again again again again again..........

--- backend/backend-app.py
+++ backend/backend-app.py
@@ -40,17 +40,17 @@ NMS_THRESHOLD = 0.4
 COLORS = np.random.uniform(0, 255, size=(len(class_names_41classes), 3))
 
 app = Flask(__name__,static_url_path='/static')
-app.config['SECRET_KEY'] = 'Lady Gaga, Bradley Cooper - Shallow (from A Star Is Born) (Official Music Video)'
+#app.config['SECRET_KEY'] = 'Lady Gaga, Bradley Cooper - Shallow (from A Star Is Born) (Official Music Video)'
 #cors = CORS(app, resources={r"/*": {"origins": "https://medwaste-ai.gezdev.com/"}})
 #app.config['CORS_ORIGINS'] = ['https://medwaste-ai.gezdev.com']
 #app.config['CORS_HEADERS'] = 'Content-Type'
-cors = CORS(app)
+#cors = CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 #CORS(app)
 
 @app.route('/')
 #@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
-@cross_origin(origin='*')
+#@cross_origin(origin='*')
 def show_index():
     str =  """<!DOCTYPE html>
 <html>
@@ -103,7 +103,7 @@ def predictYolov4_41classes(im_path):
 
 
 @app.route('/yolov4_41', methods=['POST'])
-@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
+#@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
 #@cross_origin(origin='*')
 def yolov4_41():
     im_path = ''
@@ -158,7 +158,7 @@ def predictClassify_41classes(im_path):
 
 
 @app.route('/class41', methods=['POST'])
-@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
+#@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
 #@cross_origin(origin='*')
 def classify41():
     im_path = ''
@@ -213,7 +213,7 @@ def predictClassify_4G(im_path):
 
 
 @app.route('/class4G', methods=['POST'])
-@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
+#@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
 #@cross_origin(origin='*')
 def classify4G():
     im_path = ''
