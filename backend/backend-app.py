80b7fa120a0daffb125a801b6003fe8d31a9c4df
GHOME-PC <pongsagorn.ch@gmail.com>
2022-07-01 05:29:31 +0700

    fixed cross-origin again again again again again again again.....

--- backend/backend-app.py
+++ backend/backend-app.py
@@ -44,13 +44,13 @@ app.config['SECRET_KEY'] = 'Lady Gaga, Bradley Cooper - Shallow (from A Star Is 
 #cors = CORS(app, resources={r"/*": {"origins": "https://medwaste-ai.gezdev.com/"}})
 #app.config['CORS_ORIGINS'] = ['https://medwaste-ai.gezdev.com']
 #app.config['CORS_HEADERS'] = 'Content-Type'
-#cors = CORS(app)
+cors = CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 #CORS(app)
 
 @app.route('/')
 #@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
-#@cross_origin(origin='*')
+@cross_origin(origin='*')
 def show_index():
     str =  """<!DOCTYPE html>
 <html>
@@ -103,7 +103,7 @@ def predictYolov4_41classes(im_path):
 
 
 @app.route('/yolov4_41', methods=['POST'])
-#@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
+@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
 #@cross_origin(origin='*')
 def yolov4_41():
     im_path = ''
@@ -158,7 +158,7 @@ def predictClassify_41classes(im_path):
 
 
 @app.route('/class41', methods=['POST'])
-#@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
+@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
 #@cross_origin(origin='*')
 def classify41():
     im_path = ''
@@ -213,7 +213,7 @@ def predictClassify_4G(im_path):
 
 
 @app.route('/class4G', methods=['POST'])
-#@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
+@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
 #@cross_origin(origin='*')
 def classify4G():
     im_path = ''
