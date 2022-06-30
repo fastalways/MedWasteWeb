a79550f4afd39cbbdbdc36f53b00a0ff67eed799
fastalways <pongsagorn.ch@gmail.com>
2022-07-01 00:10:18 +0700

    edit unzip for linux dl model

--- backend/model/linux_load_model.sh
+++ backend/model/linux_load_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo apt-get install zip unzip
 echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
 wget https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip
 echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
@@ -11,10 +11,10 @@ wget https://assets.gezdev.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip
 echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip'
 wget https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip
 echo 'Model Unzipping..................'
-tar -xzvf '4G-EfficientNetB5-epoch2000.pb.zip'
-tar -xzvf '41CLASSES-EfficientNetB5-epoch2000.pb.zip'
-tar -xzvf '10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
-tar -xzvf 'yolo_v4_41CLASSES.zip'
+unzip '4G-EfficientNetB5-epoch2000.pb.zip'
+unzip '41CLASSES-EfficientNetB5-epoch2000.pb.zip'
+unzip '10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
+unzip 'yolo_v4_41CLASSES.zip'
 ls -a
 Model_MedWaste_Path = $(pwd)
 echo 'Model Saved at ...' + $Model_MedWaste_Path
