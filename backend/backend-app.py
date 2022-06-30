919ffae8dedb415891bc0f654335ef560c92f2f9
GHOME-PC <pongsagorn.ch@gmail.com>
2022-07-01 04:40:11 +0700

    fixed cross-origin again again again again again again again

--- backend/backend-app.py
+++ backend/backend-app.py
@@ -42,7 +42,7 @@ COLORS = np.random.uniform(0, 255, size=(len(class_names_41classes), 3))
 app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = 'Lady Gaga, Bradley Cooper - Shallow (from A Star Is Born) (Official Music Video)'
 #cors = CORS(app, resources={r"/*": {"origins": "https://medwaste-ai.gezdev.com/"}})
-app.config['CORS_ORIGINS'] = ['https://medwaste-ai.gezdev.com']
+#app.config['CORS_ORIGINS'] = ['https://medwaste-ai.gezdev.com']
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 app.config['JSON_SORT_KEYS'] = False
@@ -50,7 +50,7 @@ CORS(app)
 
 @app.route('/')
 #@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
-@cross_origin(headers=['Content-Type'])
+@cross_origin(origin='*')
 def show_index():
     str =  """<!DOCTYPE html>
 <html>
@@ -104,7 +104,7 @@ def predictYolov4_41classes(im_path):
 
 @app.route('/yolov4_41', methods=['POST'])
 #@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
-@cross_origin(headers=['Content-Type'])
+@cross_origin(origin='*')
 def yolov4_41():
     im_path = ''
     if 'file' not in request.files:
@@ -159,7 +159,7 @@ def predictClassify_41classes(im_path):
 
 @app.route('/class41', methods=['POST'])
 #@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
-@cross_origin(headers=['Content-Type'])
+@cross_origin(origin='*')
 def classify41():
     im_path = ''
     if 'file' not in request.files:
@@ -214,7 +214,7 @@ def predictClassify_4G(im_path):
 
 @app.route('/class4G', methods=['POST'])
 #@cross_origin(origin='https://medwaste-ai.gezdev.com',headers=['Content-Type','Authorization'])
-@cross_origin()
+@cross_origin(origin='*')
 def classify4G():
     im_path = ''
     if 'file' not in request.files:
