0e32419167b686d4548e97571baeb62313996d25
Gorn@coeubserv <pongsagorn.ch@gmail.com>
2025-05-03 15:56:42 +0000

    modify group of gunicorn service

--- backend/model/linux_load_model.sh
+++ backend/model/linux_load_model.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 sudo apt-get install zip unzip  -y
 sudo apt-get install ffmpeg libsm6 libxext6  -y
-echo 'Loading Model from https://asset.coecore.com.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
-wget https://asset.coecore.com.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip
-echo 'Loading Model from https://asset.coecore.com.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
-wget https://asset.coecore.com.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip
-echo 'Loading Model from https://asset.coecore.com.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
-wget https://asset.coecore.com.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip
-echo 'Loading Model from https://asset.coecore.com.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip'
-wget https://asset.coecore.com.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip
-echo 'Loading Model from https://asset.coecore.com.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip'
-wget https://asset.coecore.com.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
+wget https://asset.coecore.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
+wget https://asset.coecore.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
+wget https://asset.coecore.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip'
+wget https://asset.coecore.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip'
+wget https://asset.coecore.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip
 echo 'Model Unzipping..................'
 unzip '4G-EfficientNetB5-epoch2000.pb.zip'
 unzip '41CLASSES-EfficientNetB5-epoch2000.pb.zip'
