be2584643c4728b596e44dcced6173535b8a5cd1
Gorn@coeubserv <pongsagorn.ch@gmail.com>
2025-05-03 14:44:26 +0000

    change asset path

--- backend/model/windows_load_model.ps1
+++ backend/model/windows_load_model.ps1
@@ -1,15 +1,15 @@
 $client = new-object System.Net.WebClient
 $CUR_DIR = Get-Location
-echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
-$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip",($CUR_DIR.Path+"\4G-EfficientNetB5-epoch2000.pb.zip"))
-echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
-$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip",($CUR_DIR.Path+"\41CLASSES-EfficientNetB5-epoch2000.pb.zip"))
-echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
-$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip",($CUR_DIR.Path+"\10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip"))
-echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip'
-$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip",($CUR_DIR.Path+"\yolo_v4_41CLASSES.zip"))
-echo 'Loading Model from https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip'
-$client.DownloadFile("https://assets.gezdev.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip",($CUR_DIR.Path+"\yolo_v5_41CLASSES.zip"))
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip'
+$client.DownloadFile("https://asset.coecore.com/model-ai/medical-waste/4G-EfficientNetB5-epoch2000.pb.zip",($CUR_DIR.Path+"\4G-EfficientNetB5-epoch2000.pb.zip"))
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip'
+$client.DownloadFile("https://asset.coecore.com/model-ai/medical-waste/41CLASSES-EfficientNetB5-epoch2000.pb.zip",($CUR_DIR.Path+"\41CLASSES-EfficientNetB5-epoch2000.pb.zip"))
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip'
+$client.DownloadFile("https://asset.coecore.com/model-ai/medical-waste/10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip",($CUR_DIR.Path+"\10FreqCLASSES-EfficientNetB5-epoch1000.pb.zip"))
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip'
+$client.DownloadFile("https://asset.coecore.com/model-ai/medical-waste/yolo_v4_41CLASSES.zip",($CUR_DIR.Path+"\yolo_v4_41CLASSES.zip"))
+echo 'Loading Model from https://asset.coecore.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip'
+$client.DownloadFile("https://asset.coecore.com/model-ai/medical-waste/yolo_v5_41CLASSES.zip",($CUR_DIR.Path+"\yolo_v5_41CLASSES.zip"))
 echo 'Model Unzipping..................'
 Expand-Archive -Path 4G-EfficientNetB5-epoch2000.pb.zip -DestinationPath ./
 Expand-Archive -Path 41CLASSES-EfficientNetB5-epoch2000.pb.zip  -DestinationPath ./
